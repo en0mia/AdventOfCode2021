a5e26bb86ac9e3f2211d3817f0f638b0e891e2c5
Simone Nicol <en0mia.dev@gmail.com>
2021-12-01 12:23:54 +0100

    :fire: Remove unused import

--- 01/src/Main.java
+++ 01/src/Main.java
@@ -4,8 +4,6 @@
     @copyright: Check the repository license.
 */
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
